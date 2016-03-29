813c62b002d96c3136aece0ba751fa577bc4ec24
Matti Schneider <matti.schneider@data.gouv.fr>
2016-03-29 12:56:13 +0200

    Adjust cards layout for older browsers

--- js/cross-browser.js
+++ js/cross-browser.js
@@ -1,7 +1,9 @@
 if (! Modernizr.flexwrap) {
 	// Semantic UI implements grid row breaks with flex-wrap, and breaks if browser doesn't support it
 	// see https://github.com/sgmap/beta.gouv.fr/issues/78
-	Array.prototype.forEach.call(document.querySelectorAll('.grid'), function(grid) {
-		grid.className = grid.className.replace('grid', '');
+	[ 'grid', 'cards' ].forEach(function(offendingClass) {
+		Array.prototype.forEach.call(document.querySelectorAll('.' + offendingClass), function(offender) {
+			offender.className = offender.className.replace(offendingClass, '');
+		});
 	});
 }
