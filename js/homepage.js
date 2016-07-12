49074ba754c71a8f577b49ca9a1fdd5d3fae9fa0
Florian Delezenne <del.florian@gmail.com>
2016-07-12 14:28:54 +0200

    Epurer la page d'accueil

--- js/homepage.js
+++ js/homepage.js
@@ -1,22 +1,21 @@
 $(function initHomepage() {
 	var currentIndex = 0,
-		total = $('#masthead .card').length;
+		total = $('#presentation .startup-presentation').length;
 
 	function selectorForIndex(index) {
 		index %= total;
-		return '#masthead .card:nth-child(' + (index + 1) + ')';
+		return '#presentation .startup-presentation:nth-child(' + (index + 1) + ')';
 	}
 
 	function showExample(event) {
-		$(selectorForIndex(currentIndex) + ',' + selectorForIndex(currentIndex + 1))
-			.transition('fade right');
+		$(selectorForIndex(currentIndex)).transition('hide');
+		$(selectorForIndex(currentIndex + 1)).transition('fade right');
 
 		currentIndex++;
 	}
 
-
-	$('#masthead .card').transition('hide');
-	$(selectorForIndex(currentIndex)).transition('browse');
+	$('#presentation .startup-presentation').transition('hide');
+	$(selectorForIndex(currentIndex)).transition('show');
 
 	document.getElementById('otherExample').addEventListener('click', showExample);
 });
