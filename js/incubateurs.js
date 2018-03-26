94776e59b005cd01336e9042a6f9dfac962ac90c
Julien DAUPHANT <jdauphant@users.noreply.github.com>
2018-03-26 12:40:24 +0200

    Ajout Marker sur carte

--- js/incubateurs.js
+++ js/incubateurs.js
@@ -4,11 +4,13 @@ function loadMap(mapId, address) {
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
             var result = JSON.parse(request.responseText);
-            var map = L.map(mapId).setView(result.features[0].geometry.coordinates.reverse(), 16);
+            var coordinates = result.features[0].geometry.coordinates.reverse();
+            var map = L.map(mapId).setView(coordinates, 16);
             L.tileLayer('https://tile.jawg.io/sunny/{z}/{x}/{y}.png?api-key=community', {
                 attribution: 'Carte &copy; <a href="http://www.jawg.io/">jawgmap</a> / <a href="https://osm.org/copyright">OpenStreetMap</a>, Adresses BAN sous licence ODbL',
                 maxZoom: 18
             }).addTo(map);
+            L.marker(coordinates).addTo(map);
       }
     };
     request.onerror = function() {};
