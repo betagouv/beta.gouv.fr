efe503450b97685fe73013e663d156f633f65e4d
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2022-06-21 15:06:39 +0200

    add startups stats page

---
+++ _pages/stats_startups.md
@@ -0,0 +1,29 @@
+---
+layout: page
+permalink: /stats_startups/
+title: Indicateurs techniques des fiches de Startup d'État
+additional_js:
+  - https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js
+  - https://unpkg.com/jquery@2.2.2/dist/jquery.min.js
+  - stats_se.js
+---
+
+## Startups d'état publiant leur budget
+
+<canvas id="budget_url"></canvas>
+
+## Startups d'état publiant leur statistiques d'usage
+
+<canvas id="stats_url"></canvas>
+
+## Startups d'état publiant leur suivi des bonnes pratiques
+
+<canvas id="dashlord_url"></canvas>
+
+## Startups ayant réalisé leur analyse des risques
+
+<canvas id="analyse_risques"></canvas>
+
+## Startups ayant réalisé leur évaluation d'accessibilité
+
+<canvas id="accessibility_status"></canvas>
---
+++ assets/additional/js/stats_se.js
@@ -0,0 +1,72 @@
+const ATTRIBUTES_PRESENCE = ["stats_url", "budget_url", "dashlord_url"];
+const ATTRIBUTES_VALUES = ["analyse_risques", "accessibility_status"];
+
+$.ajax("/api/v2.5/startups.json").done(function (response) {
+  const startupsCount = response.data.length;
+  const attributesCount = {};
+  const attributesValues = {};
+  for (const item of response.data) {
+    const startup = item.attributes;
+    console.log("startup", startup)
+    for (const attribute of ATTRIBUTES_PRESENCE) {
+      if (!startup.hasOwnProperty(attribute) || !startup[attribute])
+        continue;
+      if (!attributesCount.hasOwnProperty(attribute))
+        attributesCount[attribute] = 0;
+      attributesCount[attribute]++;
+    }
+
+    for (const attribute of ATTRIBUTES_VALUES) {
+      if (!attributesValues.hasOwnProperty(attribute))
+        attributesValues[attribute] = { "absent": 0 };
+      const value = startup.hasOwnProperty(attribute) && startup[attribute] || "absent";
+      if (!attributesValues[attribute].hasOwnProperty(value))
+        attributesValues[attribute][value] = 0
+      attributesValues[attribute][value]++;
+    }
+  }
+
+  console.log("attributesValues", attributesValues)
+
+  ATTRIBUTES_PRESENCE.forEach(attribute => {
+    new Chart(
+      document.getElementById(attribute),
+      {
+        type: "pie",
+        data: {
+          labels: [`${attribute} présent`, `${attribute} absent`],
+          datasets: [{
+            data: [
+              attributesCount[attribute],
+              startupsCount - attributesCount[attribute]
+            ],
+            backgroundColor: [
+              'rgb(54, 162, 235)',
+              'rgb(255, 99, 132)'
+            ],
+          }]
+        }
+      }
+    );
+  });
+  ATTRIBUTES_VALUES.forEach(attribute => {
+    const orderedAttributes = ["absent"].concat(Array.from(Object.keys(attributesValues[attribute]).filter(e => e !== "absent")))
+    new Chart(
+      document.getElementById(attribute),
+      {
+        type: "pie",
+        data: {
+          labels: orderedAttributes,
+          datasets: [{
+            data: orderedAttributes.map(key => attributesValues[key]),
+            backgroundColor: [
+              'rgb(255, 99, 132)',
+              'rgb(54, 162, 235)',
+              'rgb(99, 255, 132)',
+            ],
+          }]
+        }
+      }
+    );
+  });
+})
