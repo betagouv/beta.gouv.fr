cd728dcea6bfd067af4242e10f3bea984afb8d2d
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-02-09 18:14:33 +0100

    fix

--- ci/tests-fiches.mjs
+++ ci/tests-fiches.mjs
@@ -63,34 +63,27 @@ errors.forEach((error) => {
   console.error(`\t${error.errors && error.errors.map((error) => `${error.path}: ${error.received}: ${error.message.substring(0, 100)}... ${error.code || ""}`).join("\n\t")}`);
 });
 
-const checkAuthors = () => {
-  return authorsFiles.forEach((file) => {
-    const mdPath = path.join(authorsPath, `${file}.md`);
-    //  console.log(mdPath);
-    //fs.readFile(mdPath);
-    //const mdContent = await
-    const mdContent = fs.readFileSync(mdPath, "utf8");
-    //.then((data) => console.log("xxx" + data))
-    //.catch(console.error);
-    //const mdContent = await fs.readFile(mdPath, "utf8");
-    //const mdContent = "";
-    //console.log(mdContent);
-    const matterData = matter(mdContent);
-    const mdData = matterData.data;
-    if (mdData.missions && mdData.missions.length === 1 && (mdData.previously || mdData.startups)) {
-      console.log(file, mdData.previously, mdData.startups);
-      mdData.missions[0].startups = [...(mdData.previously || []), ...(mdData.startups || [])];
-      delete mdData.previously;
-      delete mdData.startups;
-      console.log(JSON.stringify(mdData));
-      const content = `${matterData.matter}\n---\n${matterData.content}`;
-      fs.writeFileSync(mdPath, content);
-    }
-    return mdData;
-  });
-};
+// fix author files missions (WIP)
+// const checkAuthors = () => {
+//   return authorsFiles.forEach((file) => {
+//     const mdPath = path.join(authorsPath, `${file}.md`);
+//     const mdContent = fs.readFileSync(mdPath, "utf8");
+//     const matterData = matter(mdContent);
+//     const mdData = matterData.data;
+//     if (mdData.missions && mdData.missions.length === 1 && (mdData.previously || mdData.startups)) {
+//       console.log(file, mdData.previously, mdData.startups);
+//       mdData.missions[0].startups = [...(mdData.previously || []), ...(mdData.startups || [])];
+//       delete mdData.previously;
+//       delete mdData.startups;
+//       console.log(JSON.stringify(mdData));
+//       const content = `${matterData.matter}\n---\n${matterData.content}`;
+//       fs.writeFileSync(mdPath, content);
+//     }
+//     return mdData;
+//   });
+// };
 
-await checkAuthors();
+// await checkAuthors();
 
 if (errors.length) {
   console.log(errors.length, "errors");
