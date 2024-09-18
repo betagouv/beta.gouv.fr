00c02d018e4cf1d5103ed90acc2fd29ad9241530
Stéphane Maniaci <stephane.maniaci@gmail.com>
2024-09-18 07:54:21 +0200

    Revert "Revert "js: introduce the Biome linter/formatter (#20754)" (#20763)"
    This reverts commit 53e7f2a46881887d7eb36c7dba1e1a620f623832.

--- assets/additional/js/search-member.js
+++ assets/additional/js/search-member.js
@@ -1,9 +1,10 @@
-const MEMBER_PLACEHOLDER = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+const MEMBER_PLACEHOLDER =
+  "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
 
-var createStartupTag = function (startupId) {
+const createStartupTag = (startupId) => {
   if (!startupId) return "";
 
-  const startup = startups.find((s) => s.id == startupId);
+  const startup = startups.find((s) => s.id === startupId);
 
   if (!startup) return "";
 
@@ -15,17 +16,17 @@ var createStartupTag = function (startupId) {
 
 function convertDate(inputFormat) {
   function pad(s) {
-    return s < 10 ? "0" + s : s;
+    return s < 10 ? `0${s}` : s;
   }
-  var d = new Date(inputFormat);
+  const d = new Date(inputFormat);
   return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join("/");
 }
 
-var createAuthorCard = function (author) {
-  var card = document.createElement("div");
+const createAuthorCard = (author) => {
+  const card = document.createElement("div");
   card.className = "fr-col-12 fr-col-sm-6 fr-col-md-4 fr-col-lg-3";
   card.id = author.id;
-  var totalStartups = [];
+  let totalStartups = [];
   if (author.startups) {
     totalStartups = totalStartups.concat(author.startups);
   }
@@ -34,13 +35,15 @@ var createAuthorCard = function (author) {
   }
   totalStartups = totalStartups.slice(0, 4);
 
-  let timestampNow = new Date();
+  const timestampNow = new Date();
   let authorEndDate;
   if (author.missions.length) {
     authorEndDate = new Date(author.missions[author.missions.length - 1].end);
   }
 
-  var avatarSrc = staticFiles.filter((staticFile) => staticFile.includes(author.id))[0];
+  let avatarSrc = staticFiles.filter((staticFile) =>
+    staticFile.includes(author.id),
+  )[0];
   if (!avatarSrc) {
     if (author.avatar) {
       avatarSrc = author.avatar;
@@ -50,12 +53,16 @@ var createAuthorCard = function (author) {
       avatarSrc = "/img/logo-generique-startup-carre-2019.jpg";
     }
   }
-  const incubator = author.incubator ? incubators.find((incubator) => incubator.id === author.incubator) : undefined;
+  const incubator = author.incubator
+    ? incubators.find((incubator) => incubator.id === author.incubator)
+    : undefined;
   const isAlumnus = authorEndDate < timestampNow;
 
   const avatar = `<img class="lozad avatar-rounded" src="${MEMBER_PLACEHOLDER}" data-src="${avatarSrc}" alt="">`;
 
-  const title = author.link ? `<a class="fr-card__link" href="${author.link}" target="_blank" rel="noopener">${author.fullname}</a>` : author.fullname;
+  const title = author.link
+    ? `<a class="fr-card__link" href="${author.link}" target="_blank" rel="noopener">${author.fullname}</a>`
+    : author.fullname;
   const detail = isAlumnus ? "Alumnus" : author.role;
   const content = author.content || "";
   const tags = totalStartups.map(createStartupTag).join("");
@@ -66,7 +73,13 @@ var createAuthorCard = function (author) {
          </a>`
       : "";
 
-  card.innerHTML = markupForCard({ title, detail, content, tags, detail, avatar });
+  card.innerHTML = markupForCard({
+    title,
+    content,
+    tags,
+    detail,
+    avatar,
+  });
 
   return card;
 };
@@ -85,9 +98,9 @@ function markupForCard(params) {
     <p class="fr-card__detail">
       ${detail}
     </p>
-    ${content ? '<p class="fr-card__desc">' + content + "</p>" : ""}
-    ${tags ? '<ul class="fr-tags-group">' + tags + "</ul>" : ""}
-    ${!!other ? '<p class="fr-card__detail">' + other + "</p>" : ""}
+    ${content ? `<p class="fr-card__desc">${content}</p>` : ""}
+    ${tags ? `<ul class="fr-tags-group">${tags}</ul>` : ""}
+    ${other ? `<p class="fr-card__detail">${other}</p>` : ""}
   </div>
 </div>
 </div>
@@ -101,65 +114,70 @@ function markupForCard(params) {
 </div>`;
 }
 
-var generateDataWithHtmlCards = function (members) {
-  var keys = Object.keys(members);
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    for (var j = 0; j < members[key].length; j++) {
-      members[key][j]["html"] = createAuthorCard(members[key][j]);
+const generateDataWithHtmlCards = (members) => {
+  const keys = Object.keys(members);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    for (let j = 0; j < members[key].length; j++) {
+      members[key][j].html = createAuthorCard(members[key][j]);
     }
   }
   return members;
 };
 
-var createIncubatorSelect = function (members, incubators, initValue) {
-  var selectIncubator = document.getElementById("select-incubateur");
-  var optionFragment = document.createDocumentFragment();
-  for (var i = 0; i < incubators.length; i++) {
-    var incubator = incubators[i];
-    var option = document.createElement("option");
+const createIncubatorSelect = (members, incubators, initValue) => {
+  const selectIncubator = document.getElementById("select-incubateur");
+  const optionFragment = document.createDocumentFragment();
+  for (let i = 0; i < incubators.length; i++) {
+    const incubator = incubators[i];
+    const option = document.createElement("option");
     option.innerText = incubator.title;
     option.value = incubator.id;
     optionFragment.appendChild(option);
   }
   selectIncubator.appendChild(optionFragment);
-  var intersection = function (array1, array2) {
-    return Boolean(
-      array1.filter(function (n) {
-        return array2.indexOf(n) !== -1;
-      }).length
+  const intersection = (array1, array2) =>
+    Boolean(array1.filter((n) => array2.indexOf(n) !== -1).length);
+  const onIncubatorChange = (value) => {
+    const selectedStartups = (
+      value ? startups.filter((d) => d.incubator_id === value) : startups
+    ).map((s) => s.id);
+    let currentsToDisplay = members.filter(
+      (member) =>
+        intersection(member.startups, selectedStartups) ||
+        member.incubator === value,
     );
-  };
-  var onIncubatorChange = function (value) {
-    var selectedStartups = (value ? startups.filter((d) => d.incubator_id === value) : startups).map((s) => s.id);
-    var currentsToDisplay = members.filter((member) => intersection(member.startups, selectedStartups) || member.incubator === value);
     currentsToDisplay = currentsToDisplay.map((d) => ({
       ...d,
       html: createAuthorCard(d),
     }));
-    var alumnisToDisplay = alumnis.filter((alumni) => intersection(alumni.startups, selectedStartups) || alumni.incubator === value);
+    let alumnisToDisplay = alumnis.filter(
+      (alumni) =>
+        intersection(alumni.startups, selectedStartups) ||
+        alumni.incubator === value,
+    );
     alumnisToDisplay = alumnisToDisplay.map((d) => ({
       ...d,
       html: createAuthorCard(d),
     }));
-    var documentFragmentCurrents = document.createDocumentFragment();
-    for (var j = 0; j < currentsToDisplay.length; j++) {
+    const documentFragmentCurrents = document.createDocumentFragment();
+    for (let j = 0; j < currentsToDisplay.length; j++) {
       documentFragmentCurrents.appendChild(currentsToDisplay[j].html);
     }
-    var gridCurrents = document.getElementsByClassName("authors")[0];
+    const gridCurrents = document.getElementsByClassName("authors")[0];
     gridCurrents.innerHTML = "";
     gridCurrents.appendChild(documentFragmentCurrents);
-    var countCurrentsElement = document.getElementById("currents-count");
+    const countCurrentsElement = document.getElementById("currents-count");
     countCurrentsElement.innerHTML = currentsToDisplay.length;
 
-    var documentFragmentAlumnis = document.createDocumentFragment();
-    for (var j = 0; j < alumnisToDisplay.length; j++) {
+    const documentFragmentAlumnis = document.createDocumentFragment();
+    for (let j = 0; j < alumnisToDisplay.length; j++) {
       documentFragmentAlumnis.appendChild(alumnisToDisplay[j].html);
     }
-    var gridAlumnis = document.getElementsByClassName("alumnis")[0];
+    const gridAlumnis = document.getElementsByClassName("alumnis")[0];
     gridAlumnis.innerHTML = "";
     gridAlumnis.appendChild(documentFragmentAlumnis);
-    var countAlumnisElement = document.getElementById("alumnis-count");
+    const countAlumnisElement = document.getElementById("alumnis-count");
     countAlumnisElement.innerHTML = alumnisToDisplay.length;
 
     if (window.lozad) {
@@ -171,11 +189,15 @@ var createIncubatorSelect = function (members, incubators, initValue) {
     selectIncubator.value = initValue;
     onIncubatorChange(initValue);
   }
-  selectIncubator.addEventListener("change", function (e) {
-    var value = e.target.value;
+  selectIncubator.addEventListener("change", (e) => {
+    const value = e.target.value;
     onIncubatorChange(value);
     const urlParams = new URLSearchParams(window.location.search);
     urlParams.set("incubateur", value);
-    history.replaceState(null, null, window.location.origin + window.location.pathname + "?" + urlParams);
+    history.replaceState(
+      null,
+      null,
+      `${window.location.origin + window.location.pathname}?${urlParams}`,
+    );
   });
 };
--- assets/additional/js/search-team.js
+++ assets/additional/js/search-team.js
@@ -1,11 +1,12 @@
-const STARTUP_PLACEHOLDER = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+const STARTUP_PLACEHOLDER =
+  "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
 
-var filters = [];
+const filters = [];
 
-var createStartupTag = function (startupId) {
+const createStartupTag = (startupId) => {
   if (!startupId) return "";
 
-  const startup = startups.find((s) => s.id == startupId);
+  const startup = startups.find((s) => s.id === startupId);
 
   if (!startup) return "";
 
@@ -15,11 +16,11 @@ var createStartupTag = function (startupId) {
     </li>`;
 };
 
-var createAuthorCard = function (author) {
-  var card = document.createElement("div");
+const createAuthorCard = (author) => {
+  const card = document.createElement("div");
   card.className = "fr-col-12 fr-col-sm-6 fr-col-md-4 fr-col-lg-3";
   card.id = author.id;
-  var totalStartups = [];
+  let totalStartups = [];
   if (author.startups) {
     totalStartups = totalStartups.concat(author.startups);
   }
@@ -28,13 +29,15 @@ var createAuthorCard = function (author) {
   }
   totalStartups = totalStartups.slice(0, 4);
 
-  let timestampNow = new Date();
+  const timestampNow = new Date();
   let authorEndDate;
   if (author.missions.length) {
     authorEndDate = new Date(author.missions[author.missions.length - 1].end);
   }
 
-  var avatarSrc = staticFiles.filter((staticFile) => staticFile.includes(author.id))[0];
+  let avatarSrc = staticFiles.filter((staticFile) =>
+    staticFile.includes(author.id),
+  )[0];
   if (!avatarSrc) {
     if (author.avatar) {
       avatarSrc = author.avatar;
@@ -44,12 +47,16 @@ var createAuthorCard = function (author) {
       avatarSrc = "/img/logo-generique-startup-carre-2019.jpg";
     }
   }
-  const incubator = author.incubator ? incubators.find((incubator) => incubator.id === author.incubator) : undefined;
+  const incubator = author.incubator
+    ? incubators.find((incubator) => incubator.id === author.incubator)
+    : undefined;
   const isAlumnus = authorEndDate < timestampNow;
 
   const avatar = `<img class="lozad avatar-rounded" src="${MEMBER_PLACEHOLDER}" data-src="${avatarSrc}" alt="">`;
 
-  const title = author.link ? `<a class="fr-card__link" href="${author.link}" target="_blank" rel="noopener">${author.fullname}</a>` : author.fullname;
+  const title = author.link
+    ? `<a class="fr-card__link" href="${author.link}" target="_blank" rel="noopener">${author.fullname}</a>`
+    : author.fullname;
   const detail = isAlumnus ? "Alumnus" : author.role;
   const content = author.content || "";
   const tags = totalStartups.map(createStartupTag).join("");
@@ -60,25 +67,30 @@ var createAuthorCard = function (author) {
          </a>`
       : "";
 
-  card.innerHTML = markupForCard({ title, detail, content, tags, detail, avatar });
+  card.innerHTML = markupForCard({
+    title,
+    content,
+    tags,
+    detail,
+    avatar,
+  });
 
   return card;
 };
 
-
-var generateDataWithHtmlCards = function (members) {
-  var keys = Object.keys(members);
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    for (var j = 0; j < members[key].length; j++) {
-      members[key][j]["html"] = createAuthorCard(members[key][j]);
+const generateDataWithHtmlCards = (members) => {
+  const keys = Object.keys(members);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    for (let j = 0; j < members[key].length; j++) {
+      members[key][j].html = createAuthorCard(members[key][j]);
     }
   }
   return members;
 };
 
-var displayNoDataMessage = function (shouldDisplay) {
-  var noDataMessage = document.getElementById("no-data-message");
+const displayNoDataMessage = (shouldDisplay) => {
+  const noDataMessage = document.getElementById("no-data-message");
   if (shouldDisplay) {
     noDataMessage.style.display = "block";
   } else {
@@ -86,62 +98,70 @@ var displayNoDataMessage = function (shouldDisplay) {
   }
 };
 
-var filterCards = function (data, value) {
-  if (filters["incubator"]) {
-    data = data.filter((d) => d.incubator_id === filters["incubator"]);
+const filterCards = (data, value) => {
+  let filtered = [];
+
+  if (filters.incubator) {
+    filtered = data.filter((d) => d.incubator_id === filters.incubator);
   }
-  return data;
+
+  return filtered;
 };
 
-var displayTeam = (team) => {
-  var $section = document.getElementById('team-'+ team.id)
-  $section.style.display = 'block'
-}
+const displayTeam = (team) => {
+  const $section = document.getElementById(`team-${team.id}`);
+  $section.style.display = "block";
+};
 
-var hideTeam = (team) => {
-  var $section = document.getElementById('team-'+ team.id)
-  $section.style.display = 'none'
-}
+const hideTeam = (team) => {
+  const $section = document.getElementById(`team-${team.id}`);
+  $section.style.display = "none";
+};
 
-var updateCards = function (authorsteam, teams) {
+const updateCards = (authorsteam, teams) => {
   displayNoDataMessage(false);
-  var teamsInIncubator = teams
-  var teamsNotInIncubator = []
-  if (filters['incubator']) {
-    teamsInIncubator = teams.filter(team => team.incubator_id === filters["incubator"])
-    teamsNotInIncubator = teams.filter(team => team.incubator_id !== filters["incubator"])
+  let teamsInIncubator = teams;
+  let teamsNotInIncubator = [];
+  if (filters.incubator) {
+    teamsInIncubator = teams.filter(
+      (team) => team.incubator_id === filters.incubator,
+    );
+    teamsNotInIncubator = teams.filter(
+      (team) => team.incubator_id !== filters.incubator,
+    );
   }
-  console.log(filters, teamsInIncubator)
+  console.log(filters, teamsInIncubator);
 
-  for (var i=0; i < teamsInIncubator.length; i++) {
-    var team = teamsInIncubator[i]
-    displayTeam(team)
+  for (let i = 0; i < teamsInIncubator.length; i++) {
+    const team = teamsInIncubator[i];
+    displayTeam(team);
   }
-  for (var i=0; i < teamsNotInIncubator.length; i++) {
-    var team = teamsNotInIncubator[i]
-    hideTeam(team)
+  for (let i = 0; i < teamsNotInIncubator.length; i++) {
+    const team = teamsNotInIncubator[i];
+    hideTeam(team);
   }
   if (!teamsInIncubator.length) {
-    displayNoDataMessage(true)
+    displayNoDataMessage(true);
   }
 };
 
-var createIncubatorSelect = function (authorsteam, teams, incubators, initValue) {
-  var selectIncubator = document.getElementsByClassName("select-operateur");
-  var optionFragment = document.createDocumentFragment();
-  for (var i = 0; i < incubators.length; i++) {
-    var incubator = incubators[i];
-    var option = document.createElement("option");
+const createIncubatorSelect = (authorsteam, teams, incubators, initValue) => {
+  const selectIncubator = document.getElementsByClassName("select-operateur");
+  const optionFragment = document.createDocumentFragment();
+  for (let i = 0; i < incubators.length; i++) {
+    const incubator = incubators[i];
+    const option = document.createElement("option");
     option.innerText = incubator.title;
     option.value = incubator.id;
     optionFragment.appendChild(option);
   }
   selectIncubator.appendChild(optionFragment);
-  var onIncubatorChange = function (value) {
-    filters["incubator"] = value;
-    var incubatorElements = document.getElementsByClassName("incubator-header");
-    for (var i = 0; i < incubatorElements.length; i++) {
-      var incubatorElement = incubatorElements[i];
+  const onIncubatorChange = (value) => {
+    filters.incubator = value;
+    const incubatorElements =
+      document.getElementsByClassName("incubator-header");
+    for (let i = 0; i < incubatorElements.length; i++) {
+      const incubatorElement = incubatorElements[i];
       if (incubatorElement.id !== value) {
         incubatorElement.style.display = "none";
       } else {
@@ -154,11 +174,15 @@ var createIncubatorSelect = function (authorsteam, teams, incubators, initValue)
     selectIncubator.value = initValue;
     onIncubatorChange(initValue);
   }
-  selectIncubator.addEventListener("change", function (e) {
-    var value = e.target.value;
+  selectIncubator.addEventListener("change", (e) => {
+    const value = e.target.value;
     onIncubatorChange(value);
     const urlParams = new URLSearchParams(window.location.search);
     urlParams.set("incubateur", value);
-    history.replaceState(null, null, window.location.origin + window.location.pathname + "?" + urlParams);
+    history.replaceState(
+      null,
+      null,
+      `${window.location.origin + window.location.pathname}?${urlParams}`,
+    );
   });
 };
