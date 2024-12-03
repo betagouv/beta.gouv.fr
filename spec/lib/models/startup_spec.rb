4e4d93c61ecb3c7289b433afb000ca37f6ead9ee
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2024-12-03 11:29:24 +0100

    lib: expose the phases

--- spec/lib/models/startup_spec.rb
+++ spec/lib/models/startup_spec.rb
@@ -16,6 +16,8 @@ describe Beta::Startup do
   phases:
     - name: investigation
       start: 2023-12-22
+    - name: construction
+      start: 2024-01-01
 YAML
   end
 
@@ -24,6 +26,26 @@ YAML
   %w[title phases mission incubator].each do |attr|
     it "defines and updates a '#{attr}' attribute" do
       expect(startup.public_send(attr)).not_to be_nil
+    end
+  end
+
+  describe 'phases' do
+    it 'can retrieve a specific phase' do
+      expect(startup.investigation_phase).to be_present
+    end
+
+    context "when the phase isn't present" do
+      it 'returns nil' do
+        expect(startup.acceleration_phase).to be_nil
+      end
+    end
+
+    it 'can tell which phase is active' do
+      expect(startup).to be_in_construction
+    end
+
+    it "doesn't use the end date to figure out the active phase" do
+      expect(startup).not_to be_in_acceleration
     end
   end
 end
