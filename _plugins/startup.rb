a0f6e97c4e18777d3a8573ba60f7914d60b6ebf8
Stéphane Maniaci <stephane.maniaci@beta.gouv.fr>
2026-06-12 15:22:55 +0200

    startup: fix the badge for special events (FAST, national impact)
    It was expecting the last event to be national impact, when it can be
    anywhere in the list. Use a plugin to allow events to produce a badge
    regardless.

--- _plugins/startup.rb
+++ _plugins/startup.rb
@@ -4,6 +4,22 @@ require_relative '../lib/models/member'
 
 module Jekyll
   module StartupFilter
+    # rubocop:disable Style/ParallelAssignment
+    # looks fine here
+    def to_startup_badge(event)
+      case event['name']
+      when 'fast'
+        type, name = 'new', 'Lauréat FAST'
+      when 'national_impact'
+        type, name = 'success', 'Service à impact national'
+      else
+        return
+      end
+
+      "<span class=\"fr-badge fr-badge--#{type} fr-mr-1w\">#{name}</span>"
+    end
+    # rubocop:enable Style/ParallelAssignment
+
     def members(all_members, startup_id, alumni = false)
       id = startup_id.delete_prefix('/startups/')
 
